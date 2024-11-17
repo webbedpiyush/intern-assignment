2aa707fa0b7b9568a5adf0b8f9500576f042bdd1
Piyush <piyushtiwarindls220@gmail.com>
2024-11-17 19:48:39 +0530

    env added modification

--- dist/utils/firebase.js
+++ dist/utils/firebase.js
@@ -22,10 +22,27 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.auth = exports.db = void 0;
 const admin = __importStar(require("firebase-admin"));
-const serviceAccount = require("../../functions/secret.json");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Handle newlines in private key
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
+// const serviceAccount = require("../../secret.json") as ServiceAccount;
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,7 +1,36 @@
 import * as admin from "firebase-admin";
 import { ServiceAccount } from "firebase-admin";
+import dotenv from "dotenv";
 
-const serviceAccount = require("../../secret.json") as ServiceAccount;
+dotenv.config();
+
+interface CustomServiceAccount extends ServiceAccount {
+  type: string;
+  projectId: string;
+  privateKeyId: string;
+  privateKey: string;
+  clientEmail: string;
+  clientId: string;
+  authUri: string;
+  tokenUri: string;
+  authProviderX509CertUrl: string;
+  clientX509CertUrl: string;
+}
+
+const serviceAccount: CustomServiceAccount = {
+  type: process.env.FIREBASE_TYPE!,
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID!,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"), // Handle newlines in private key
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+  clientId: process.env.FIREBASE_CLIENT_ID!,
+  authUri: process.env.FIREBASE_AUTH_URI!,
+  tokenUri: process.env.FIREBASE_TOKEN_URI!,
+  authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL!,
+  clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL!,
+};
+
+// const serviceAccount = require("../../secret.json") as ServiceAccount;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
